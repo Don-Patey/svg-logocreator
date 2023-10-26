6d9389fbe86076e16a6e9a54707178c803d10098
Don-Patey <139824991+Don-Patey@users.noreply.github.com>
2023-10-26 19:29:18 -0300

    remove code creating duplicate text in logo

--- index.js
+++ index.js
@@ -1,86 +1,75 @@
-const fs = require(`fs`);
-const inquirer = require(`inquirer`);
-const {Circle} = require(`./shape/shapes.js`);
+const fs = require('fs');
+const inquirer = require('inquirer');
+const { Circle, Square, Triangle } = require('./shape/shapes.js');
 
-
-// set txt need to play around x&y to get it in the middle
-
-class Svg{
-    constructor(){
-        this.text = "";
-        this.shapeDesign = ""; 
+class Svg {
+    constructor() {
+        this.text = '';
+        this.shapeDesign = '';
     }
-    render(){
+    render() {
         return `<svg width="300" height="200" xmlns="http://www.w3.org/2000/svg">
     ${this.shapeDesign}
     ${this.text}
     </svg>`;
     }
-    setText(text, color){
-        this.text = `<text x="0" y="15" fill="${color}">${text}</text>`;
+    setText(text, Textcolor, x, y) {
+      this.text = `<text x="${x}" y="${y}" fill="${Textcolor}">${text}</text>`; 
     }
-    setShape(shape){
+    setShape(shape) {
         this.shapeDesign = shape.render();
     }
 }
 
-
-
-
-
-    //ask for text, text color, shape color, //
-    inquirer.prompt([
+inquirer
+    .prompt([
         {
             type: 'list',
             name: 'shape',
-            message: "what shape would you like to make?",
+            message: 'What shape would you like to make?',
             choices: ['Square', 'Triangle', 'Circle'],
         },
         {
             type: 'input',
             name: 'text',
-            message: "what text would you like in the logo?",
-            validate: (input) => 
-                input.length <= 3 || "Text should be up to 3 text characters"
+            message: 'What text would you like in the logo?',
+            validate: (input) =>
+                input.length <= 3 || 'Text should be up to 3 text characters',
         },
         {
             type: 'input',
             name: 'textColor',
-            message: "what text color would you like to use?",
+            message: 'What text color would you like to use?',
         },
         {
             type: 'input',
             name: 'shapeColor',
-            message: "what shape color would you like to use?",
-         
+            message: 'What shape color would you like to use?',
         }
-    ]).then(({shape, text, textColor, shapeColor}) => {
-    let answer
+    ])
+    .then(({ shape, text, textColor, shapeColor }) => {
+        let shapeObj;
 
-       
+        if (shape === 'Circle') {
+            shapeObj = new Circle(shapeColor, textColor, text);
+        } else if (shape === 'Square') {
+            shapeObj = new Square(shapeColor, textColor, text);
+        } else {
+            shapeObj = new Triangle(shapeColor, textColor, text);
+        }
 
-      
-        // if (shape === 'Circle'){
-        //    answer = new Circle();
-        // } else if (shape === 'Square'){
-        //     svg.setShape(new Square(shapeColor));
-        // } else {
-        //     svg.setShape(new Triangle(shapeColor));
-      
-        answer = new Circle();
-        answer.setColor(shapeColor);
         const svg = new Svg();
-        svg.setText(text, textColor);
-        svg.setShape(answer);
-        
 
-        
+        // Calculate x and y for text to be centered
+       const x = 150 - (text.length * 10);
+       const y = 100;
 
-       fs.writeFile('logo.svg', svg.render(), (err) => {
+      svg.setText(text, textColor, x, y);
+        svg.setShape(shapeObj); //
+
+        fs.writeFile('logo.svg', svg.render(), (err) => {
             if (err) {
                 console.log(err);
-            } 
-        })
-    }) 
-
-    
+            }
+        });
+    });
--- shape/shapes.js
+++ shape/shapes.js
@@ -1,16 +1,35 @@
-class Shape{
-    constructor() {
-     this.color = "";  
-    }
-   setColor(color){
-       this.color = color;
+class Shape {
+    constructor(color, textColor, text) {
+        this.color = color;
+        this.textColor = textColor;
+        this.text = text;
+}
+}
+
+class Circle extends Shape {
+    render() {
+        return `<svg width="200" height="200">
+        <circle cx="100" cy="100" r="50" stroke-width="1" fill="${this.color}" />
+        </svg>`;
     }
 }
 
-class Circle extends Shape{
-    render(){
-        return `<circle cx="50" cy="50" r="50" fill="${this.color}"/>`
+class Triangle extends Shape {
+    render() {
+        return `<svg width="200" height="200">
+        <polygon points="100,0 160,100 40,100" fill="${this.color}" />
+        
+        </svg>`;
+        console.log(this.text);
     }
 }
 
-module.exports = {Circle};
+class Square extends Shape {
+    render() {
+        return `<svg width="200" height="200">
+        <rect x="50" y="100" width="100" height="100" fill="${this.color}" stroke-width="1" />
+        </svg>`;
+    }
+}
+
+module.exports = { Circle, Square, Triangle };
